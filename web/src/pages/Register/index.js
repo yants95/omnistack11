e2211461012345e258537db64aaa96888db822cd
Yan Soares <yts.1995@gmail.com>
2020-03-28 20:08:54 -0300

    fixing text error

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -48,7 +48,7 @@ export default function Register() {
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho cadastro
+                        Já tenho cadastro
                     </Link>
                 </section>
 
